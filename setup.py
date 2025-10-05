03c70177a7677785bbd07e4cadc8a626b9613953
Maithri Rao <maithrirao30@gmail.com>
2025-10-05 22:11:19 +0200

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Maithri',
+    author_email= 'maithrirao30@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
